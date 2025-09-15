696337217c588dbb22a081fc01df3c881ba4fed8
Durgesh Kumar Meena <58581435+durgeshmeena@users.noreply.github.com>
2025-09-16 01:01:28 +0530

    add nvim config

---
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,9 @@
+-- ref: https://github.com/cpow/neovim-for-newbs/blob/main/lua/plugins/treesitter.lua
+
+return {
+  "nvim-treesitter/nvim-treesitter",
+  -- enable auto install
+  opts = {
+    auto_install = true,
+  },
+}
